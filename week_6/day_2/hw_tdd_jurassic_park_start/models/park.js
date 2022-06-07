07c98453db580dae579deb81562e713defc33cb4
Brian Kerr <briantkerr91@gmail.com>
2022-06-07 20:55:21 +0100

    MVP homework done

--- week_6/day_2/hw_tdd_jurassic_park_start/models/park.js
+++ week_6/day_2/hw_tdd_jurassic_park_start/models/park.js
@@ -16,4 +16,50 @@ Park.prototype.removeDinosaur = function(dinosaur){
     this.dinosaurs.splice(index, 1)
 };
 
+Park.prototype.highestViewingDino = function(){
+
+    var highest = 0
+    for (var dinosaur of this.dinosaurs){
+        if (dinosaur.guestsAttractedPerDay > highest){
+            highest = dinosaur.guestsAttractedPerDay
+        }
+    }
+    return highest
+
+};
+
+Park.prototype.findDinoSpecies = function(species){
+
+    let fullList = []
+    for (var dinosaur of this.dinosaurs){
+        if (dinosaur.species === species){
+            fullList.push(dinosaur)
+        }
+    };
+    return fullList
+
+};
+
+Park.prototype.totalVisitorsPerDay = function(){
+
+    let total = 0
+    for (var dinosaur of this.dinosaurs){
+        total += dinosaur.guestsAttractedPerDay
+    };
+    return total
+
+};
+
+Park.prototype.totalVisitorsPerYear = function(){
+
+    return this.totalVisitorsPerDay() * 365
+
+};
+
+Park.prototype.totalYearRevenue = function(){
+
+    return this.totalVisitorsPerYear() * this.ticketPrice;
+
+};
+
 module.exports = Park;
--- week_6/day_2/hw_tdd_jurassic_park_start/specs/park_spec.js
+++ week_6/day_2/hw_tdd_jurassic_park_start/specs/park_spec.js
@@ -57,17 +57,61 @@ describe('Park', function() {
     const actual = park.dinosaurs;
     const expected = [dinosaur2]
     assert.deepEqual(actual, expected)
-    
+
   });
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+  it('should be able to find the dinosaur that attracts the most visitors', function(){
 
-  it('should be able to find all dinosaurs of a particular species');
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    const actual = park.highestViewingDino();
+    const expected = 50;
+    assert.strictEqual(actual, expected)
 
-  it('should be able to calculate the total number of visitors per day');
+  });
 
-  it('should be able to calculate the total number of visitors per year');
+  it('should be able to find all dinosaurs of a particular species', function(){
 
-  it('should be able to calculate total revenue for one year');
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    const actual = park.findDinoSpecies('t-rex');
+    const expected = [dinosaur1]
+    assert.deepEqual(actual, expected)
+
+  });
+
+  it('should be able to calculate the total number of visitors per day', function(){
+
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    const actual = park.totalVisitorsPerDay();
+    const expected = 115;
+    assert.strictEqual(actual, expected)
+
+  });
+
+  it('should be able to calculate the total number of visitors per year', function(){
+
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    const actual = park.totalVisitorsPerYear();
+    const expected = 41975;
+    assert.strictEqual(actual, expected)
+
+  });
+
+  it('should be able to calculate total revenue for one year', function(){
+
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    const actual = park.totalYearRevenue();
+    const expected = 1049375;
+    assert.strictEqual(actual, expected)
+  });
 
 });
